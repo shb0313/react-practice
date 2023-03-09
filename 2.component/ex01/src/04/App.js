8f293b45267a3f1dbf76d0b76cff120b92b286d2
shb7815 <shb7815@gmail.com>
2023-03-09 10:03:47 +0900

    component styling

--- 2.component/ex01/src/04/App.js
+++ 2.component/ex01/src/04/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
     <div id='App'>
       <MyComponent 
-        props02={10}/>
+        props02={10}
+        props03={true}
+        props04={{no:1, name:'둘리'}}     
+        props05={['hello', 'world', 'react.js', 'es6']}     
+        props06={() => '함수'}
+        props07={10}
+        props08={[true, false, true, false]}
+        props09={{no:1, name:'둘리', email:'dooly@email.com'}}
+        />
     </div>
   );
 }
--- 2.component/ex01/src/04/MyComponent.js
+++ 2.component/ex01/src/04/MyComponent.js
@@ -1,7 +1,7 @@
 import React, {Fragment } from 'react';
 import {PropTypes} from 'prop-types';
 
-function MyComponent({props01, props02}) {
+function MyComponent({props01, props02, props03, props04, props05, props06, props07, props08, props09}) {
     return (
         <Fragment>
             <h2>Property Validation</h2>
@@ -11,6 +11,38 @@ function MyComponent({props01, props02}) {
 
             <span>props02: {props02 ? props02 : '--- not set ---'}</span>
             <br/>
+            
+            <span>props03: {props03 ? `${props03}` : '--- not set ---'}</span>
+            <br/>
+
+            <span>props04: {props04 ? props04.name : '--- not set ---'}</span>
+            <br/>
+
+            <span>props05: {props05.map((e, i) => <b key={i}>{e}</b>)}</span>
+            <br/>
+
+            <span>props06: {props06 ? props06() : '--- not set ---'}</span>
+            <br/>
+
+            <span>props07: {props07 ? props07 : '--- not set ---'}</span>
+            <br/>
+            
+            <span>props08: {props08.map((e, i) => <b key={i}>{`${e}`}</b>)}</span>
+            <br/>
+
+            <span>
+                {'props09:'}
+                {
+                    props09 ?
+                        <div>
+                            <h3>{props09.no}</h3>
+                            <h4>{props09.name}</h4>
+                            <h5>{props09.email}</h5>
+                        </div>
+                        :
+                        <strong>{'--- not set'}</strong>
+                }
+            </span>
 
         </Fragment>
     );
@@ -18,13 +50,34 @@ function MyComponent({props01, props02}) {
 
 // property type and required
 MyComponent.propTypes = {
+
+    //javascript Data required
     props01: PropTypes.string,
-    props02: PropTypes.number.isRequired
+    props02: PropTypes.number.isRequired,
+    props03: PropTypes.bool.isRequired,
+    props04: PropTypes.object.isRequired,
+    props05: PropTypes.array.isRequired,
+    props06: PropTypes.func.isRequired,
+
+    // Combined with javascript data tyoe
+    props07: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).inRequired,
+    props08: PropTypes.arrayOf(PropTypes.bool).inRequired,
+    props09: PropTypes.shape({
+        no: PropTypes.number.isRequired,
+        name:  PropTypes.string,
+        email:  PropTypes.string
+    }).inRequired
+
 }
 
 // property default value
 MyComponent.defaultProps = {
-    props01: '기본값'
+    props01: '기본값',
+    props02: 10,
+    props03: false,
+    props04: {},
+    props05: [],
+    props06: () => {}    // dummy function
 }
 
 export default MyComponent;
