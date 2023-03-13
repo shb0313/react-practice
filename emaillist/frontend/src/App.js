e6ef960374ddc75e6d37462ab63ea83877a9069d
shb7815 <shb7815@gmail.com>
2023-03-13 16:49:14 +0900

    emaillist

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,27 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import RegisterForm from './RegisterForm';
-import SearchBar from './SearchBar';
-import Emaillist from './Emaillist';
-import './assets/scss/App.scss';
-import data from './assets/json/data.json'
+import React, { useState, useEffect } from "react";
+import RegisterForm from "./RegisterForm";
+import SearchBar from "./SearchBar";
+import Emaillist from "./Emaillist";
+import "./assets/css/App.css";
+import data from "./assets/json/data.json";
 
 const App = () => {
   const [emails, setEmails] = useState(data);
-  const notifyKeyWordChanged = function(keyword) {
+  const notifyKeyWordChanged = function (keyword) {
     // keyword가 firstName or lastName or email
-    const newEmails = emails.filter(function(e) {
-        return
+    const newEmails = emails.filter(function (e) {
+      return;
     });
-
-  }
+  };
 
   return (
-    <div id={'App'}>
+    <div id={"App"}>
       <RegisterForm />
       <SearchBar />
       <Emaillist emails={emails} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
